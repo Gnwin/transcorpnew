7a77fcebaae13f1faf3e07e09f0031a46d5c1308
Godwin Nwachukwu <nwgwin@gmail.com>
2020-01-12 14:05:58 +0100

    removed flex from news class and added function to class n3ii

--- js/app.js
+++ js/app.js
@@ -25,3 +25,14 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
+let bookingPrice = document.querySelectorAll(".n3ii .a");
+let bookingLodgePreview = document.querySelector(".n3i img");
+
+let bookingPics = ["assets/img/rectangle8Copy.png", "assets/img/rectangle9Copy2@3x.png", "assets/img/rectangle9Copy3@3x.png", "assets/img/rectangle4Copy72@3x.png", "assets/img/rectangle4Copy73@2x.png"]
+
+for (let i = 0; i < bookingPrice.length; i++) {
+  bookingPrice[i].addEventListener("click", ()=>{
+    bookingLodgePreview.src = bookingPics[i];
+  })
+}
